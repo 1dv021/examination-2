1bb9a0f57b42d77c5f331feaebc6df7434b4d015
mtslck <mats.loock@lnu.se>
2017-06-27 12:49:13 +0200

    Squashed 'examination-1/' content from commit 978047c
    git-subtree-dir: examination-1
    git-subtree-split: 978047cd58f5af0439676dec5e6752477a67f33e

---
+++ src/statistics.js
@@ -0,0 +1,33 @@
+/**
+ * Module for obtaining descriptive information about a set of data.
+ *
+ * @author TODO: Write your name here.
+ * @version 1.1.0
+ */
+
+'use strict'
+
+/**
+ * Returns the descriptive information (maximum, mean, median, minimum,
+ * mode, range and standard deviation) from a set of numbers.
+ *
+ * @param {number[]} data The set of data to be analyzed.
+ * @throws {TypeError} Argument must be an array.
+ * @throws {Error} Argument can not be an empty array.
+ * @returns {{maximum: number, mean: number, median: number, minimum: number, mode: number[], range: number, standardDeviation: number}}
+ */
+function descriptiveStatistics (data) {
+    // TODO: Write your code here.
+}
+
+// TODO: Write your code here.
+
+// Exports
+exports.descriptiveStatistics = descriptiveStatistics
+exports.maximum = undefined
+exports.mean = undefined
+exports.median = undefined
+exports.minimum = undefined
+exports.mode = undefined
+exports.range = undefined
+exports.standardDeviation = undefined
